3b6f0c5e896ad5038a280258a64093363131cabc
Trey Bruce <jab3@korazin.net>
2024-11-30 18:19:51 -0600

    feat: add tests for loading conv from db

--- pkg/database/sqlite3_test.go
+++ pkg/database/sqlite3_test.go
@@ -28,6 +28,7 @@ func TestNewDB(t *testing.T) {
 }
 
 func TestInsertConversation(t *testing.T) {
+	RemoveDB()
 	db, err := NewDB(dbPath, dbTable)
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
@@ -46,7 +47,12 @@ func TestClose(t *testing.T) {
 	db.Close()
 }
 
+func RemoveDB() {
+	os.Remove(dbPath)
+}
+
 func TestInsertConversationWithError(t *testing.T) {
+	RemoveDB()
 	db, err := NewDB(dbPath, dbTable)
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
@@ -59,6 +65,25 @@ func TestInsertConversationWithError(t *testing.T) {
 	// 0.123 is technically invalid.
 	// err = db.InsertConversation("prompt", "response", "", 0.0)
 	// assert.NotNil(t, err)
+
+	db.Close()
+}
+
+func TestLoadConversations(t *testing.T) {
+	db, err := NewDB(dbPath, dbTable)
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+
+	err = db.InsertConversation("prompt", "response", "model_name", 0.5, 10, 20, 1)
+	assert.Nil(t, err)
+	err = db.InsertConversation("prompt2", "response2", "model_name2", 0.5, 10, 20, 2)
+	assert.Nil(t, err)
+
+	conversations, err := db.LoadConversationFromDB(1)
+	assert.Nil(t, err)
+	// Len 2 because one prompt/response turn in the DB is one row; however,
+	// it's two LLMConversations, which LoadConversationFromDB does.
+	assert.Len(t, conversations, 2)
 
 	db.Close()
 }
